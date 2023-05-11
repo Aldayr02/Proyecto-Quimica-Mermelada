82c0af84834684d970db279806f86cd1b388850b
Aldayr02 <id724577@iteso.mx>
2023-05-11 17:54:42 -0600

    Update de Operation.js agregando utilidades el return y update del excel

--- Operations.js
+++ Operations.js
@@ -1,6 +1,6 @@
-var PrecioManzana;
-var PrecioAzucar;
-var PrecioMermelada;
+var PrecioManzana = 50;
+var PrecioAzucar = 20;
+var PrecioMermelada = 100;
 
 function CallPrice(PrecioMan, PrecioAz, PrecioMer) {
     PrecioManzana = PrecioMan;
@@ -25,18 +25,21 @@ function InputManzanas(Manzanas) {
     let Azucar = ManzanaMachacada / 2;
     let Mermelada = Manzanas * (325/436);
 
-    return {"Azucar":{"Kg":Azucar, "Precio":Azucar * PrecioAzucar}, "Mermelada":{"Kg":Mermelada, "Precio": Mermelada * PrecioMermelada}};
+    let Egreso = (Azucar * PrecioAzucar) + (Manzanas * PrecioManzana);
+    let Ingreso = Mermelada * PrecioMermelada;
+
+    return {"Azucar":{"Kg":Azucar, "Precio":Azucar * PrecioAzucar}, "Mermelada":{"Kg":Mermelada, "Precio": Mermelada * PrecioMermelada}, "Balance":{"Egresos": Egreso, "Ingreso":Ingreso, "Utilidad": Ingreso - Egreso}};
 }
 
 function InputMermelada(Mermelada) {
 
     //Proceso 1
     let Manzanas = Mermelada * (436/325)
-    let Residuos1 = Manzanas * (((44*100)/436)/100);
+    let Residuos1 = Manzanas * (((47*100)/436)/100);
     let ManzanasPeladas = Manzanas - Residuos1;
 
     //Proceso 2
-    let Residuos2 = ManzanasPeladas * (((22*100)/399)/100);
+    let Residuos2 = ManzanasPeladas * (((24*100)/389)/100);
     let ManzanasPicadas = ManzanasPeladas - Residuos2;
 
     //Proceso 3
@@ -45,19 +48,22 @@ function InputMermelada(Mermelada) {
 
     //Proceso 4
     let Azucar = ManzanaMachacada / 2;
+    
+    let Egreso = (Azucar * PrecioAzucar) + (Manzanas * PrecioManzana);
+    let Ingreso = Mermelada * PrecioMermelada;
 
-    return {"Manzanas":{"Kg":Manzanas, "Precio":Manzanas * PrecioManzana}, "Azucar":{"Kg":Azucar, "Precio": Azucar * PrecioAzucar}};
+    return {"Manzanas":{"Kg":Manzanas, "Precio":Manzanas * PrecioManzana}, "Azucar":{"Kg":Azucar, "Precio": Azucar * PrecioAzucar}, "Balance":{"Egresos": Egreso, "Ingreso":Ingreso, "Utilidad": Ingreso - Egreso}};
     
 }
 
 function InputAzucar(Azucar) {
     //Proceso 1
     let ManzanasConstante = 436
-    let Residuos1 = ManzanasConstante * (((44*100)/436)/100);
+    let Residuos1 = ManzanasConstante * (((47*100)/436)/100);
     let ManzanasPeladas = ManzanasConstante - Residuos1;
 
     //Proceso 2
-    let Residuos2 = ManzanasPeladas * (((22*100)/399)/100);
+    let Residuos2 = ManzanasPeladas * (((24*100)/389)/100);
     let ManzanasPicadas = ManzanasPeladas - Residuos2;
 
     //Proceso 3
@@ -71,8 +77,12 @@ function InputAzucar(Azucar) {
 
     let Mermelada = Manzanas * (325/436);
 
-    return {"Manzanas":{"Kg":Manzanas, "Precio":Manzanas * PrecioManzana}, "Mermelada":{"Kg":Mermelada, "Precio": Mermelada * PrecioMermelada}};
+    let Egreso = (Azucar * PrecioAzucar) + (Manzanas * PrecioManzana);
+    let Ingreso = Mermelada * PrecioMermelada;
+
+    return {"Manzanas":{"Kg":Manzanas, "Precio":Manzanas * PrecioManzana}, "Mermelada":{"Kg":Mermelada, "Precio": Mermelada * PrecioMermelada}, "Balance":{"Egresos": Egreso, "Ingreso":Ingreso, "Utilidad": Ingreso - Egreso}};
 }
+
 
 console.log(InputAzucar(100));
 console.log(InputManzanas(500));
