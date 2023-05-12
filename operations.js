466efda14ba1a075c323d59b1cc0e072439e9f75
joseAcevesG <jose.aceves@iteso.mx>
2023-05-12 10:52:18 -0600

    correccion de g a kg

--- operations.js
+++ operations.js
@@ -9,9 +9,9 @@ function putPrice(precioMan, precioAzu, precioMer) {
 }
 
 function inputManzanas(manzanas) {
-	manzanas = manzanas * 1000
+	
+	manzanas*=1000;
 	//Proceso 1
-	manzanas = manzanas *1000;
 	let residuos1 = manzanas * ((47 * 100) / 436 / 100);
 	let manzanasPeladas = manzanas - residuos1;
 
@@ -24,21 +24,26 @@ function inputManzanas(manzanas) {
 	let manzanaMachacada = manzanasPicadas - residuos3;
 
 	//Proceso 4
-	let azucar = (manzanaMachacada / 2) / 1000;
-	let mermelada = (manzanas * (325 / 436)) / 1000;
+	let azucar = (manzanaMachacada / 2);
+	let mermelada = (manzanas * (325 / 436));
 
-	manzanas /= 1000
+	azucar/=1000;
+	mermelada/=1000;
+	manzanas/=1000;
+
 	let egreso = azucar * precioAzucar + manzanas * precioManzana;
 	let ingreso = mermelada * precioMermelada;
 
-	return {manzanas:{kg: manzanas/1000 , precio: manzanas/1000 * precioManzana}, azucar: { kg: azucar/1000 , precio: azucar/1000 * precioAzucar }, mermelada: { kg: mermelada/1000 , precio: mermelada/1000 * precioMermelada }, balance: { egresos: egreso, ingresos: ingreso, utilidad: ingreso - egreso } };
+
+
+	return {manzanas:{kg: manzanas , precio: manzanas * precioManzana}, azucar: { kg: azucar , precio: azucar * precioAzucar }, mermelada: { kg: mermelada , precio: mermelada * precioMermelada }, balance: { egresos: egreso, ingresos: ingreso, utilidad: ingreso - egreso } };
 }
  
 function inputMermelada(mermelada) {
-	mermelada = mermelada * 1000
 	//Proceso 1
+	mermelada*=1000;
 
-	let manzanas = (mermelada * (436 / 325)) / 1000;
+	let manzanas = mermelada * (436 / 325);
 
 
 	let residuos1 = manzanas * ((47 * 100) / 436 / 100);
@@ -53,19 +58,23 @@ function inputMermelada(mermelada) {
 	let manzanaMachacada = manzanasPicadas - residuos3;
 
 	//Proceso 4
-	let azucar = (manzanaMachacada / 2) / 1000;
+	let azucar = (manzanaMachacada / 2);
 
-	mermelada /= 1000;
+
+	azucar/=1000;
+	mermelada/=1000;
+	manzanas/=1000;
+
 	let egreso = azucar * precioAzucar + manzanas * precioManzana;
 	let ingreso = mermelada * precioMermelada; 
 
-	return {manzanas:{kg: manzanas/1000 , precio: manzanas/1000 * precioManzana}, azucar: { kg: azucar/1000 , precio: azucar/1000 * precioAzucar }, mermelada: { kg: mermelada/1000 , precio: mermelada/1000 * precioMermelada }, balance: { egresos: egreso, ingresos: ingreso, utilidad: ingreso - egreso } };
+	return {manzanas:{kg: manzanas , precio: manzanas * precioManzana}, azucar: { kg: azucar , precio: azucar * precioAzucar }, mermelada: { kg: mermelada , precio: mermelada * precioMermelada }, balance: { egresos: egreso, ingresos: ingreso, utilidad: ingreso - egreso } };
 }
 
 function inputAzucar(azucar) {
-	azucar = azucar * 1000;
+	azucar*=1000;
+
 	//Proceso 1
-	azucar *= 1000
 	let manzanasConstante = 436;
 	let residuos1 = manzanasConstante * ((47 * 100) / 436 / 100);
 	let manzanasPeladas = manzanasConstante - residuos1;
@@ -81,15 +90,18 @@ function inputAzucar(azucar) {
 	//Proceso 4
 	let azucarConstante = manzanaMachacada / 2;
 
-	let manzanas = ((azucar * 436) / azucarConstante) / 1000;
+	let manzanas = ((azucar * 436) / azucarConstante);
 
-	let mermelada = (manzanas * (325 / 436)) / 1000;
+	let mermelada = (manzanas * (325 / 436));
 
-	azucar /= 1000;
+	azucar/=1000;
+	mermelada/=1000;
+	manzanas/=1000;
+
 	let egreso = azucar * precioAzucar + manzanas * precioManzana;
 	let ingreso = mermelada * precioMermelada;
 
-	return {manzanas:{kg: manzanas/1000 , precio: manzanas/1000 * precioManzana}, azucar: { kg: azucar/1000 , precio: azucar/1000 * precioAzucar }, mermelada: { kg: mermelada/1000 , precio: mermelada/1000 * precioMermelada }, balance: { egresos: egreso, ingresos: ingreso, utilidad: ingreso - egreso } };
+	return {manzanas:{kg: manzanas , precio: manzanas * precioManzana}, azucar: { kg: azucar , precio: azucar * precioAzucar }, mermelada: { kg: mermelada , precio: mermelada * precioMermelada }, balance: { egresos: egreso, ingresos: ingreso, utilidad: ingreso - egreso } };
 }
    
 exports.inputManzanas = inputManzanas;
